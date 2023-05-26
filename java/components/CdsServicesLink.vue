730426e73241aec153e2f9341521e53b3edcee11
Christian Georgi <christian.georgi@sap.com>
2023-05-26 12:39:09 +0200

    Link components for Java

---
+++ java/components/CdsServicesLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <a
+    :href="`https://www.javadoc.io/doc/com.sap.cds/cds-services-api/${version}/${link}`"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <slot />
+  </a>
+</template>
+
+<script setup>
+
+  const props = defineProps({
+    link: String,
+    latest: Boolean
+  })
+
+  const version = props.latest ? 'latest' : '1.34.1'
+
+</script>

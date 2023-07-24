ed081756dab8bf371cb0f9d7f967491803009d91
Christian Georgi <chgeo@users.noreply.github.com>
2023-07-24 15:29:15 +0200

    Search: boost or downrate pages (Java/Node) (#318)
    Downrate Java pages if Node toggle is set and vice versa.
    This does not address pages w/ mixed content, but improves the search
    for pure Java/Node pages.
    
    Example is search for `draft` that only shows the relevant pages.
    
    Also exclude old page from search

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -41,6 +41,7 @@ const config:UserConfig<CapireThemeConfig> = {
     search: {
       provider: 'local',
       options: {
+        exclude: (relativePath) => relativePath.includes('/customization-old'),
         miniSearch: {
           options: {
             tokenize: text => text.split( /[\n\r #%*,=/:;?[\]{}()&]+/u ), // simplified charset: removed [-_.@] and non-english chars (diacritics etc.)
@@ -59,6 +60,18 @@ const config:UserConfig<CapireThemeConfig> = {
               }
               return term
             },
+          },
+          searchOptions: {
+            //@ts-ignore
+            boostDocument: (documentId, term, storedFields:Record<string, string|string[]>) => {
+              // downrate matches in archives, changelogs etc.
+              if (documentId.match(/\/archive|changelog|old-mtx-apis/)) return -5
+              // downrate Java matches if Node is toggled and vice versa
+              const toggled = localStorage.getItem('impl-variant')
+              if (toggled === 'node' && (documentId.includes('/java/')    || storedFields?.titles?.includes('Java')))    return -1
+              if (toggled === 'java' && (documentId.includes('/node.js/') || storedFields?.titles?.includes('Node.js'))) return -1
+              return 1
+            }
           }
         }
       }

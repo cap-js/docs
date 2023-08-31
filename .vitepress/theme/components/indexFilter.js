cap-js/docs

commit 98e45668db555a8e0a09fca21d9d92aeb24a4d1c

Fix double base in URL
Changed since Vitepress rc.4

.vitepress/theme/components/indexFilter.js
@@ -1,5 +1,4 @@
-const { base, themeConfig: { sidebar }} = global.VITEPRESS_CONFIG.site
-import { join } from 'node:path'
+const { themeConfig: { sidebar }} = global.VITEPRESS_CONFIG.site
 
 export default (pages, basePath) => {
   let items = findInItems(basePath, sidebar) || []
@@ -20,7 +19,7 @@ export default (pages, basePath) => {
     .map(p => {
       // this data is inlined in each index page, so sparsely construct the final object
       return {
-        url : join(base, p.url),
+        url: p.url,
         title: p.title,
         frontmatter: {
           synopsis: p.frontmatter.synopsis

commit 1ea0d3298fa967e122556675f938cb3a4ef31755

Restore order in index page
This got broken with 98e4566, so that during sorting, we compared paths
with and without base paths.

.vitepress/theme/components/indexFilter.js
@@ -1,9 +1,10 @@
-const { themeConfig: { sidebar }} = global.VITEPRESS_CONFIG.site
+const { base, themeConfig: { sidebar }} = global.VITEPRESS_CONFIG.site
+import { join } from 'node:path'
 
 export default (pages, basePath) => {
   let items = findInItems(basePath, sidebar) || []
   items = items.map(item => { return { ...item, link: item.link.replace(/\.md$/, '') }})
-  const itemLinks = items.map(item => item.link)
+  const itemLinks = items.map(item => join(base, item.link))
 
   return pages
     .map(p => {
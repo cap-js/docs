1f9d2abf9baedf2fd1b11c7088678d038622d135
Mara Kiefer <mara.kiefer@sap.com>
2024-04-08 16:12:51 +0200

    More lint-specific class names

--- .vitepress/theme/components/RulesRefTable.vue
+++ .vitepress/theme/components/RulesRefTable.vue
@@ -18,66 +18,66 @@
 </script>
 
 <template>
-    <table class="ref-table">
+    <table class="lint-ref-table">
         <thead>
             <tr>
                 <th class="col-prop">Recommended</th>
                 <th class="col-prop">Fixable</th>
                 <th class="col-prop">Sugguestions</th>
                 <th class="col-prop" v-if="category !== 'Environment'">Editor</th>
-                <th class="col-rule">Rule</th>
+                <th class="lint-rule">Rule</th>
             </tr>
         </thead>
         <tr v-for="entry in data[category]">
-            <td class="col-prop">
-                <text class="text-prop">{{ entry.isRecommended }}</text>
+            <td class="lint-rule-prop">
+                <text class="lint-prop-symbol">{{ entry.isRecommended }}</text>
             </td>
-            <td class="col-prop">
-                <text class="text-prop">
+            <td class="lint-rule-prop">
+                <text class="lint-prop-symbol">
                     {{ entry.hasFix }}
                 </text>
             </td>
-            <td class="col-prop">
-                <text class="text-prop">
+            <td class="lint-rule-prop">
+                <text class="lint-prop-symbol">
                     {{ entry.hasSuggestions }}
                 </text>
             </td>
             <td class="col-prop" v-if="category !== 'Environment'">
-                <text class="text-prop">
+                <text class="lint-prop-symbol">
                     {{ entry.model }}
                 </text>
             </td>
-            <td class="col-rule">
+            <td class="lint-rule">
                 <span v-if="!entry.url" class="col-name">{{ entry.rule }}</span>
                 <a :href="entry.url" v-if="!!entry.url">{{ entry.rule }}</a>
                 <br>
-                <text class="text-desc">{{ entry.description }}</text>
+                <text class="lint-rule-desc">{{ entry.description }}</text>
             </td>
         </tr>
     </table>
 </template>
 
 <style scoped>
-.ref-table {
+.lint-ref-table {
     display: block
 }
-.text-desc {
+.lint-rule-desc {
     font-size: small;
 }
-.col-rule {
+.lint-rule {
     width: 70%;
     line-height: 120%;
 }
-.col-name {
+.lint-rule-name {
     font-weight: 500;
     color: #7d7d7d;
 }
-.col-prop {
+.lint-rule-prop {
     width: 5% !important;
     text-align: center; 
     vertical-align: middle;
 }
-.text-prop {
+.lint-rule-symbolprop {
     text-align: center;
 }
 </style>

d2f4db075a6e7bf77a82246fff34b5d83958f9b6
Christian Georgi <christian.georgi@sap.com>
2024-04-17 16:19:23 +0200

    Add title/tooltip

--- .vitepress/theme/components/Since.vue
+++ .vitepress/theme/components/Since.vue
@@ -1,5 +1,5 @@
 <template>
-  <Badge type="info" :text="`since ${of} ${version}`" />
+  <Badge type="info" :text="`since ${of} ${version}`" :title="`This feature is only available with ${of} version ${version} or higher`" />
 </template>
 
 <script setup lang="ts">

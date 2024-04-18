53c4ab6feb787412dade4a91ea7b9045f2e0b44d
Christian Georgi <christian.georgi@sap.com>
2024-04-18 16:38:56 +0200

    Use _requires_

--- .vitepress/theme/components/Since.vue
+++ .vitepress/theme/components/Since.vue
@@ -1,5 +1,5 @@
 <template>
-  <Badge type="info" :text="`since ${of} ${version}`" :title="`This feature is only available with ${of} version ${version} or higher`" />
+  <Badge type="info" :text="`requires ${of} ${version}`" :title="`This feature is only available as of ${of} version ${version} or higher`" />
 </template>
 
 <script setup lang="ts">

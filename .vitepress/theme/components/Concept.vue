fe8a6b8cd3969cb4c6e4fb04943860aafad5c3c9
Christian Georgi <chgeo@users.noreply.github.com>
2024-04-26 14:44:15 +0200

    Explain concept badge, allow more badges (#863)
    Explain concept badge

--- .vitepress/theme/components/Concept.vue
+++ .vitepress/theme/components/Concept.vue
@@ -1,7 +1,7 @@
 <template>
   <Badge
     type="warning"
-    text="Concept"
-    title="Concept"
+    text="concept"
+    title="Concept features are ideas for potential future enhancements and an opportunity for you to give feedback. This is not a commitment to implement the feature though."
   />
 </template>

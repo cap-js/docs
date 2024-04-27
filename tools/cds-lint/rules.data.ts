2c0eb5222b75b9c090f958de316d5db52f561b20
Christian Georgi <christian.georgi@sap.com>
2024-04-27 12:44:38 +0200

    Use ESLint 9 and eslint-plugin-cds 3 in examples

--- tools/cds-lint/rules.data.ts
+++ tools/cds-lint/rules.data.ts
@@ -7,25 +7,25 @@ export default {
     let plugin: any;
     try {
       // @ts-ignore
-        plugin = await import('@sap/eslint-plugin-cds');
+        plugin = (await import('@sap/eslint-plugin-cds')).default;
       } catch (e) {
         return data
       }
-      const allRules = Object.keys(plugin?.configs.all.rules).sort();
+      const allRules = Object.keys(plugin.configs.all.rules).sort();
 
       allRules.forEach((rule: string) => {
         rule = rule.replace('@sap/cds/', '');
         // TODO: Skip the min-node-version rule as it will be deprecated soon
         if (rule !== 'min-node-version') {
-          const description = plugin?.rules[rule]?.meta.docs.description;
+          const description = plugin.rules[rule]?.meta.docs.description;
           const ruleDocs = path.join(__dirname, `meta/${rule}.md`)
           const hasRuleDocs = fs.existsSync(ruleDocs)
           const url = hasRuleDocs ? `./meta/${rule}` : null
-          const isRecommended = plugin?.rules[rule]?.meta.docs.recommended ? '✅' : '';
-          const hasFix = plugin?.rules[rule]?.meta.fixable ? '🔧' : '';
-          const hasSuggestions = plugin?.rules[rule]?.meta.hasSuggestions ? '💡' : '';
-          const model = plugin?.rules[rule]?.meta?.model === 'parsed' ? '👀' : '';
-          const category = plugin?.rules[rule]?.meta?.model === 'none' ? "Environment" : "Model Validation";
+          const isRecommended = plugin.rules[rule]?.meta.docs.recommended ? '✅' : '';
+          const hasFix = plugin.rules[rule]?.meta.fixable ? '🔧' : '';
+          const hasSuggestions = plugin.rules[rule]?.meta.hasSuggestions ? '💡' : '';
+          const model = plugin.rules[rule]?.meta?.model === 'parsed' ? '👀' : '';
+          const category = plugin.rules[rule]?.meta?.model === 'none' ? "Environment" : "Model Validation";
           data[category].push({ rule, description, url, isRecommended, hasFix, hasSuggestions, model })
         }
       })

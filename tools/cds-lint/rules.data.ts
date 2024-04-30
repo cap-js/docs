149ac7fb89c516ba2e35da9b4fd487935146e199
Christian Georgi <christian.georgi@sap.com>
2024-04-30 11:26:49 +0200

    Beautify MD code blocks

--- tools/cds-lint/rules.data.ts
+++ tools/cds-lint/rules.data.ts
@@ -6,29 +6,33 @@ export default {
     const data: any = { "Model Validation": [], "Environment": [] };
     let plugin: any;
     try {
-      // @ts-ignore
-        plugin = (await import('@sap/eslint-plugin-cds')).default;
-      } catch (e) {
-        return data
-      }
-      const allRules = Object.keys(plugin.configs.all.rules).sort();
+      plugin = (await import('@sap/eslint-plugin-cds')).default;
+    } catch (e) {
+      return data
+    }
+    const allRules = Object.keys(plugin.configs.all.rules).sort();
 
-      allRules.forEach((rule: string) => {
-        rule = rule.replace('@sap/cds/', '');
-        // TODO: Skip the min-node-version rule as it will be deprecated soon
-        if (rule !== 'min-node-version') {
-          const description = plugin.rules[rule]?.meta.docs.description;
-          const ruleDocs = path.join(__dirname, `meta/${rule}.md`)
-          const hasRuleDocs = fs.existsSync(ruleDocs)
-          const url = hasRuleDocs ? `./meta/${rule}` : null
-          const isRecommended = plugin.rules[rule]?.meta.docs.recommended ? '✅' : '';
-          const hasFix = plugin.rules[rule]?.meta.fixable ? '🔧' : '';
-          const hasSuggestions = plugin.rules[rule]?.meta.hasSuggestions ? '💡' : '';
-          const model = plugin.rules[rule]?.meta?.model === 'parsed' ? '👀' : '';
-          const category = plugin.rules[rule]?.meta?.model === 'none' ? "Environment" : "Model Validation";
-          data[category].push({ rule, description, url, isRecommended, hasFix, hasSuggestions, model })
-        }
-      })
+    allRules.forEach((rule: string) => {
+      rule = rule.replace('@sap/cds/', '');
+      const description = md2Html(plugin.rules[rule]?.meta.docs.description);
+      const ruleDocs = path.join(__dirname, `meta/${rule}.md`)
+      const hasRuleDocs = fs.existsSync(ruleDocs)
+      const url = hasRuleDocs ? `./meta/${rule}` : null
+      const isRecommended = plugin.rules[rule]?.meta.docs.recommended ? '✅' : '';
+      const hasFix = plugin.rules[rule]?.meta.fixable ? '🔧' : '';
+      const hasSuggestions = plugin.rules[rule]?.meta.hasSuggestions ? '💡' : '';
+      const model = plugin.rules[rule]?.meta?.model === 'parsed' ? '👀' : '';
+      const category = plugin.rules[rule]?.meta?.model === 'none' ? "Environment" : "Model Validation";
+      data[category].push({ rule, description, url, isRecommended, hasFix, hasSuggestions, model })
+    })
     return data;
   }
+
+}
+
+function md2Html(string:string='') {
+  return string
+    // @ts-ignore
+    .replaceAll(/`(.*?)`/g, '<code>$1</code>')
+    .replaceAll(/(https?:\/\/.*?)(\s)/g, '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>$2')
 }

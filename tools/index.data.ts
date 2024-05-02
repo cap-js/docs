e66737907ad71e6ee38eea62286b49fff2c4dbf1
Christian Georgi <christian.georgi@sap.com>
2024-05-02 13:45:30 +0200

    More patterns for tools index

--- tools/index.data.ts
+++ tools/index.data.ts
@@ -4,7 +4,12 @@ import filter from '../.vitepress/theme/components/indexFilter.ts'
 
 const basePath = basename(__dirname)
 
-export default createContentLoader([`**/${basePath}/*.md`, `**/${basePath}/**/index.md`], {
+export default createContentLoader([
+  `**/${basePath}/*.md`,
+  `**/${basePath}/**/index.md`,
+  `**/cds-dk.md`,
+  `**/hybrid-testing.md`,
+], {
   transform(rawData) {
     return filter(rawData, `/${basePath}/`)
   }

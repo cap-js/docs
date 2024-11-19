ae1087963224fb0164a2b87584ebdc67ac2af74a
Daniel Hutzel <daniel.hutzel@sap.com>
2024-11-19 08:42:14 +0100

    Copy errors

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // Add back-to-top behavior to the outline's 'On this page' title
-// const e = document.getElementById('doc-outline-aria-label')
-// if (e) e.onclick = ()=> document.location = '#'
+const e = document.getElementById('doc-outline-aria-label')
+if (e) e.onclick = ()=> document.location = '#'
 
 // Restore the last selection re Node/Java
 // const v = localStorage.getItem('impl-variant')

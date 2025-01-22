cap-js/docs

commit 546a70867dbb8e68c1a6b66245492cd0b591b7d8

Simplified impl variants classes Using `.node` and `.java` instead of `.impl.node` and `.impl.java`. But the latter still works as well

.vitepress/lib/md-attrs-propagate.ts
@@ -4,7 +4,7 @@ import { MarkdownRenderer } from 'vitepress'
  * Propagates `impl java|node` classes that are set on a header
  * down to sub headers. Example:
  * ```md
- * # Header 1 {.impl .node}
+ * # Header 1 {.node}
  * Text
  * ## Header 2
  * ```

.vitepress/theme/components/implvariants/ImplVariants.vue
@@ -111,7 +111,7 @@ function toggleContent(variant) {
 // All other DOM content is handled at build time on MD level (see md-attrs-propagate.ts)
 function markOutlineItems() {
   const hashes = {}
-  const impls = document.querySelectorAll('.impl.node, .impl.java')
+  const impls = document.querySelectorAll('.node, .java')
   for (let each of impls) {
     hashes['#' + each.id] = each
   }
@@ -125,7 +125,6 @@ function markOutlineItems() {
 }
 
 function markClasses(el, classes) {
-  el.classList.add('impl') // in IE, add() only accepts one element
   if (classes.contains('node'))   el.classList.add('node')
   if (classes.contains('java'))   el.classList.add('java')
 }

.vitepress/theme/styles.scss
@@ -468,12 +468,12 @@ tr:nth-child(odd) {
 }
 
 html.java {
-  & node, & .node {
+  & .node {
     display: none;
   }
 }
 html.node {
-  & java, & .java {
+  & .java {
     display: none;
   }
 }

commit 2f9c4f5b155b28f788bff2352920a015d9170d4a

Fix variant selection in headers (#1584)
Got broken with 546a708

.vitepress/lib/md-attrs-propagate.ts
@@ -1,7 +1,7 @@
 import { MarkdownRenderer } from 'vitepress'
 
 /**
- * Propagates `impl java|node` classes that are set on a header
+ * Propagates `java|node` classes that are set on a header
  * down to sub headers. Example:
  * ```md
  * # Header 1 {.node}
@@ -10,12 +10,12 @@ import { MarkdownRenderer } from 'vitepress'
  * ```
  * Will result in HTML like this
  * ```html
- * <h1 class="impl node">
- * <p class="impl node">Text</p>
- * <h2 class="impl node">
+ * <h1 class="node">
+ * <p class="node">Text</p>
+ * <h2 class="node">
  * ```
  */
-export function install(md: MarkdownRenderer, classRegex=/impl (node|java)/) {
+export function install(md: MarkdownRenderer, classRegex=/\b(node|java)\b/) {
 
   // propagates class in sub headers as token.attrs
   md.core.ruler.push('propagate_attrs', (state) => {